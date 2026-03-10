23ff4180472e4763f4f235be970aa0d2dbbc53e3
Rafael Adl Zarabi <rafaelzarabi@gmail.com>
2026-03-10 12:07:50 +0100

    Timeslot.cs klasse

---
+++ 123123/Timeslot.cs
@@ -0,0 +1,26 @@
+﻿//Vores enums til tid og dag
+
+
+// TimeSlot.cs
+namespace _123123
+{
+    internal enum TimeSlot
+    {
+        Morgen,
+        Formiddag,
+        Eftermiddag
+    }
+}
+
+// Day.cs
+namespace _123123
+{
+    internal enum Day
+    {
+        Mandag,
+        Tirsdag,
+        Onsdag,
+        Torsdag,
+        Fredag
+    }
+}
